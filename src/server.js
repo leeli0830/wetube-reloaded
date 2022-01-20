c08d05e43b6ae8de9df411644435a7f99a8faffa
leeli0830 <leeli0830@gmail.com>
2022-01-19 22:33:48 -0800

    Add Respond & Response handler

--- src/server.js
+++ src/server.js
@@ -2,9 +2,17 @@ import express from "express";
 
 const PORT = 4000;
 
-const app = express();
+const app = express(); // Create Express instance
+
+const handleHome = (req, res) => {
+  // First parameter is for request and Second parameter is for response
+  return res.end(); // Kill response
+  // return res.send("I still love you.") // This returns message
+};
+
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port: http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening); // Listening to requests on specific port
