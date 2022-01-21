1e187d68610a7525605b031db19d4660c67a2fc8
leeli0830 <leeli0830@gmail.com>
2022-01-20 20:54:54 -0800

    Add an example middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,18 @@ const PORT = 4000;
 
 const app = express(); // Create Express instance
 
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
   // First parameter is for request and Second parameter is for response
   return res.end(); // Kill response
-  // return res.send("I still love you.") // This returns message
 };
 
-app.get("/", handleHome);
+app.use(gossipMiddleware); // Every route uses this middleware
+app.get("/", gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port: http://localhost:${PORT}`);
